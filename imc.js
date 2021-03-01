54e1e01b46073e6be9592939044b332896befa9a
Vagner Augusto Lima <professor.vagneraugusto@gmail.com>
2021-03-01 11:53:34 -0300

    Mensagem de atualização

---
+++ imc.js
@@ -0,0 +1,53 @@
+const calcular = document.getElementById('calcular');
+
+function updateTextPeso(val) {
+    document.getElementById('peso').value=val; 
+  }
+  function updateTextAltura(val) {
+    document.getElementById('altura').value=val; 
+  }
+
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+  
+
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        resultado.style.display = "flex";
+
+
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Continue Assim';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II.';
+        }else {
+            classificacao = 'com obesidade grau III. Cuidado!!!';
+        }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+        
+    }else {
+        resultado.style.display = "flex";
+        resultado.textContent = 'Preencha todos os campos!!!';
+    }
+
+}
+
+
+
+calcular.addEventListener('click', imc);
